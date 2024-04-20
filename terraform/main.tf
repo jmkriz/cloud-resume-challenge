9c967724a3628e5d6855a8ef8eb43cf0b346f2ad
jmkriz <61602169+jmkriz@users.noreply.github.com>
2024-04-20 17:09:48 -0400

    Create Terraform for DynamoDB table

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket = "jmkriz"
+    key    = "terraform/backend"
+    region = "us-east-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_dynamodb_table" "resume_visitor_count" {
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "count"
+  name         = "jmkriz-resume-visitor-count"
+
+  attribute {
+    name = "count"
+    type = "N"
+  }
+}
