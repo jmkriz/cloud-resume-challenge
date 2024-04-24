3770ef15827bf534c975ab53c16ea5d20c471cfc
jmkriz <61602169+jmkriz@users.noreply.github.com>
2024-04-23 22:25:41 -0400

    Add mock credentials to tests

--- tests/test_visitor_counter.py
+++ tests/test_visitor_counter.py
@@ -1,11 +1,20 @@
 import boto3
 import moto
+import os
 import pytest
 
 from src import visitor_counter
 
 @pytest.fixture
-def dummy_table():
+def dummy_credentials():
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = 'testing'
+    os.environ["AWS_SESSION_TOKEN"] = 'testing'
+    os.environ["AWS_DEFAULT_REGION"] = 'us-east-1'
+
+@pytest.fixture
+def dummy_table(dummy_credentials):
     with moto.mock_dynamodb():
         client = boto3.client("dynamodb")
         client.create_table(
