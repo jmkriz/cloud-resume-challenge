e79d8b77d7316e298d70d028e8a8b2ebb852a602
jmkriz <61602169+jmkriz@users.noreply.github.com>
2024-04-27 17:21:21 -0400

    Refactor tests

--- tests/test_visitor_counter.py
+++ tests/test_visitor_counter.py
@@ -3,8 +3,10 @@ import moto
 import os
 import pytest
 
+from contextlib import contextmanager
 from src import visitor_counter
 
+# Fake AWS credentials, to ensure tests don't accidentally affect prod
 @pytest.fixture
 def dummy_credentials():
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
@@ -14,40 +16,65 @@ def dummy_credentials():
     os.environ["AWS_DEFAULT_REGION"] = 'us-east-1'
 
 @pytest.fixture
-def dummy_table(dummy_credentials):
+def dummy_dynamodb_client(dummy_credentials):
     with moto.mock_dynamodb():
-        client = boto3.client("dynamodb")
-        client.create_table(
-            AttributeDefinitions = [
-                {"AttributeName": "id", "AttributeType": "S"}
-            ],
-            TableName = "resume",
-            KeySchema = [
-                {"AttributeName": "id", "KeyType": "HASH"}
-            ],
-            BillingMode="PAY_PER_REQUEST"
-        )
+        client = boto3.client('dynamodb')
+        yield client
 
-        yield "resume"
+@contextmanager
+def dummy_table(dummy_dynamodb_client, **kwargs):
+    table_key = kwargs['table_key'] if 'table_key' in kwargs else 'id'
+    table_name = kwargs['table_name'] if 'table_name' in kwargs else 'resume'
+    key_value = kwargs['key_value'] if 'key_value' in kwargs else 'visitors'
+    count_name = kwargs['count_name'] if 'count_name' in kwargs else 'visitor_count'
+    count_value = kwargs['count_value'] if 'count_value' in kwargs else 0
 
-@pytest.fixture
-def zero_table(dummy_table):
-    table = boto3.resource("dynamodb").Table(dummy_table)
-    table.put_item(Item={"id": "visitors", "visitor_count": 0})
-    yield table
+    dummy_dynamodb_client.create_table(
+        AttributeDefinitions = [
+            {'AttributeName': table_key, 'AttributeType': 'S'}
+        ],
+        TableName = table_name,
+        KeySchema = [
+            {'AttributeName': table_key, 'KeyType': 'HASH'}
+        ],
+        BillingMode = 'PAY_PER_REQUEST'
+    )
+    table = boto3.resource('dynamodb').Table(table_name)
+    table.put_item(Item={table_key: key_value, count_name: count_value})
+    yield
 
-@pytest.fixture
-def fortytwo_table(dummy_table):
-    table = boto3.resource("dynamodb").Table(dummy_table)
-    table.put_item(Item={"id": "visitors", "visitor_count": 42})
-    yield table
+def test_zero(dummy_dynamodb_client):
+    with dummy_table(dummy_dynamodb_client):
+        assert visitor_counter.lambda_handler({'httpMethod': 'GET'}, None)['body'] == 0
+        assert visitor_counter.lambda_handler({'httpMethod': 'PUSH'}, None)['statusCode'] == 200
+        assert visitor_counter.lambda_handler({'httpMethod': 'GET'}, None)['body'] == 1
 
-def test_zero(zero_table):
-    assert int(visitor_counter.get_visitor_count(zero_table)['Item']['visitor_count']) == 0
-    visitor_counter.increment_visitor_count(zero_table)
-    assert int(visitor_counter.get_visitor_count(zero_table)['Item']['visitor_count']) == 1
+def test_fortytwo(dummy_dynamodb_client):
+    with dummy_table(dummy_dynamodb_client, count_value = 42):
+        assert visitor_counter.lambda_handler({'httpMethod': 'GET'}, None)['body'] == 42
+        assert visitor_counter.lambda_handler({'httpMethod': 'PUSH'}, None)['statusCode'] == 200
+        assert visitor_counter.lambda_handler({'httpMethod': 'GET'}, None)['body'] == 43
 
-def test_fortytwo(fortytwo_table):
-    assert int(visitor_counter.get_visitor_count(fortytwo_table)['Item']['visitor_count']) == 42
-    visitor_counter.increment_visitor_count(fortytwo_table)
-    assert int(visitor_counter.get_visitor_count(fortytwo_table)['Item']['visitor_count']) == 43
+def test_no_table(dummy_dynamodb_client):
+    with dummy_table(dummy_dynamodb_client, table_name = 'test'):
+        assert visitor_counter.lambda_handler({'httpMethod': 'GET'}, None)['statusCode'] == 400
+        assert visitor_counter.lambda_handler({'httpMethod': 'PUSH'}, None)['statusCode'] == 400
+
+def test_wrong_key(dummy_dynamodb_client):
+     with dummy_table(dummy_dynamodb_client, table_key = 'test'):
+        assert visitor_counter.lambda_handler({'httpMethod': 'GET'}, None)['statusCode'] == 400
+        assert visitor_counter.lambda_handler({'httpMethod': 'PUSH'}, None)['statusCode'] == 400
+
+def test_wrong_key_value(dummy_dynamodb_client):
+    with dummy_table(dummy_dynamodb_client, key_value = 'test'):
+        assert visitor_counter.lambda_handler({'httpMethod': 'GET'}, None)['body'] is None
+        assert visitor_counter.lambda_handler({'httpMethod': 'PUSH'}, None)['statusCode'] == 400
+
+def test_wrong_count_column_name(dummy_dynamodb_client):
+    with dummy_table(dummy_dynamodb_client, count_name = 'test'):
+        assert visitor_counter.lambda_handler({'httpMethod': 'GET'}, None)['body'] is None
+        assert visitor_counter.lambda_handler({'httpMethod': 'PUSH'}, None)['statusCode'] == 400
+
+def test_method_not_allowed(dummy_dynamodb_client):
+    with dummy_table(dummy_dynamodb_client):
+        assert visitor_counter.lambda_handler({'httpMethod': 'DELETE'}, None)['statusCode'] == 405
