48947a2a36308c4fdcf209970a541fac60f779e9
jmkriz <61602169+jmkriz@users.noreply.github.com>
2024-04-29 20:52:21 -0400

    Add API Gateway and IAM permissions for Lambda to Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,6 +72,28 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+data "aws_iam_policy_document" "access_visitor_count_doc" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:UpdateItem"
+    ]
+    resources = [aws_dynamodb_table.resume_visitor_count.arn]
+  }
+}
+
+resource "aws_iam_policy" "access_visitor_count" {
+  name        = "access-visitor-count"
+  description = "Policy for Lambda to access the visitor count"
+  policy      = data.aws_iam_policy_document.access_visitor_count_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "visitor_count_attachment" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.access_visitor_count.arn
+}
+
 data "archive_file" "lambda_handler" {
   type = "zip"
 
@@ -118,4 +140,79 @@ resource "aws_lambda_function" "visitor_counter" {
   runtime = "python3.9"
 
   code_signing_config_arn = aws_lambda_code_signing_config.visitor_counter_csc.arn
+}
+
+# REST API Gateway to call Lambda function
+resource "aws_api_gateway_rest_api" "visitor_counter" {
+  name = "visitor_count"
+}
+
+resource "aws_api_gateway_resource" "visitor_counter_resource" {
+  rest_api_id = aws_api_gateway_rest_api.visitor_counter.id
+  parent_id   = aws_api_gateway_rest_api.visitor_counter.root_resource_id
+  path_part   = "visitorcount"
+}
+
+resource "aws_api_gateway_method" "get_visitor_count" {
+  rest_api_id   = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id   = aws_api_gateway_resource.visitor_counter_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "increment_visitor_count" {
+  rest_api_id   = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id   = aws_api_gateway_resource.visitor_counter_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get_visitor_count_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id             = aws_api_gateway_resource.visitor_counter_resource.id
+  http_method             = aws_api_gateway_method.get_visitor_count.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.visitor_counter.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "increment_visitor_count_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id             = aws_api_gateway_resource.visitor_counter_resource.id
+  http_method             = aws_api_gateway_method.increment_visitor_count.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.visitor_counter.invoke_arn
+}
+
+resource "aws_lambda_permission" "visitor_counter_lambda_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.visitor_counter.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.visitor_counter.execution_arn}/*"
+}
+
+resource "aws_api_gateway_deployment" "visitor_counter_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.visitor_counter.id
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.visitor_counter_resource.id,
+      aws_api_gateway_method.get_visitor_count.id,
+      aws_api_gateway_method.increment_visitor_count.id,
+      aws_api_gateway_integration.get_visitor_count_integration.id,
+      aws_api_gateway_integration.increment_visitor_count_integration.id
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "visitor_counter_stage" {
+  deployment_id = aws_api_gateway_deployment.visitor_counter_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.visitor_counter.id
+  stage_name    = "visitor_counter"
 }
