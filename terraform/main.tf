c392f4106a1a9ace926de8c4e0b2f266b81f1fa7
jmkriz <61602169+jmkriz@users.noreply.github.com>
2024-05-13 17:48:19 -0400

    Add CloudFront and DNS configuration, as well as S3 website content, to Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -260,3 +260,178 @@ resource "aws_s3_bucket_website_configuration" "front_end" {
     suffix = "index.html"
   }
 }
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.bucket.bucket
+  key = "index.html"
+  source = "frontend/index.html"
+}
+
+resource "aws_s3_object" "styles" {
+  bucket = aws_s3_bucket.bucket.bucket
+  key = "styles.css"
+  source = "frontend/styles.css"
+}
+
+# CloudFront distribution and DNS
+
+locals {
+  s3_origin_id       = "s3_bucket"
+  api_origin_id      = "api_gateway"
+  custom_domain_name = "jmkriz.dev"
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = local.custom_domain_name
+  subject_alternative_names = ["www.${local.custom_domain_name}", "api.${local.custom_domain_name}"]
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_zone" "frontend_zone" {
+  name = local.custom_domain_name
+}
+
+resource "aws_route53_record" "frontend_dns" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.frontend_zone.zone_id
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.frontend_dns : record.fqdn]
+}
+
+resource "aws_cloudfront_distribution" "frontend_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+  }
+
+  /* origin {
+    domain_name = aws_api_gateway_stage.visitor_counter_stage.invoke_url
+    origin_id   = local.api_origin_id
+  } */
+
+  default_cache_behavior {
+    allowed_methods  = ["HEAD", "GET", "OPTIONS"]
+    cached_methods   = ["HEAD", "GET", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  /* ordered_cache_behavior {
+    path_pattern     = "/api/*"
+    allowed_methods  = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
+    cached_methods   = ["HEAD", "GET", "OPTIONS"
+    target_origin_id = local.api_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 360
+    max_ttl                = 3600
+  } */
+
+  restrictions {
+    geo_restriction {
+      locations        = []
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.cert.arn
+    cloudfront_default_certificate = false
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method = "sni-only"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+  aliases             = [local.custom_domain_name, "www.${local.custom_domain_name}"]
+  price_class         = "PriceClass_100"
+}
+
+resource "aws_api_gateway_domain_name" "api" {
+  certificate_arn = aws_acm_certificate_validation.cert_validation.certificate_arn
+  domain_name = "api.${local.custom_domain_name}"
+}
+
+resource "aws_api_gateway_base_path_mapping" "api_mapping" {
+  api_id = aws_api_gateway_rest_api.visitor_counter.id
+  stage_name = aws_api_gateway_stage.visitor_counter_stage.stage_name
+  domain_name = aws_api_gateway_domain_name.api.domain_name
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.frontend_zone.zone_id
+  name = "www.${local.custom_domain_name}"
+  type = "A"
+
+  alias {
+    name = aws_cloudfront_distribution.frontend_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.frontend_distribution.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "root" {
+  zone_id = aws_route53_zone.frontend_zone.zone_id
+  name = local.custom_domain_name
+  type = "A"
+
+  alias {
+    name = aws_route53_record.www.name
+    zone_id = aws_route53_record.www.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "api" {
+  zone_id = aws_route53_zone.frontend_zone.zone_id
+  name = aws_api_gateway_domain_name.api.domain_name
+  type = "A"
+
+  alias {
+    name = aws_api_gateway_domain_name.api.cloudfront_domain_name
+    zone_id = aws_api_gateway_domain_name.api.cloudfront_zone_id
+    evaluate_target_health = true
+  }
+}
